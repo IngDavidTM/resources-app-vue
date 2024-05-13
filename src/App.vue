659927a56221663d404cb867cf25017b1a1b7563
IngDavidTM <david-1011@hotmail.com>
2024-05-13 14:04:23 -0500

    Add style and stored resources

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,17 @@
 <template>
   <ul>
-    <learning-resource
-      v-for="res in storedResources"
-      :key="res.id"
-      :title="res.title"
-      :description="res.description"
-      :url="res.url"
-    ></learning-resource>
+    <stored-resources
+      :resources="storedResources"
+    ></stored-resources>
   </ul>
 </template>
 
 <script>
-import LearningResource from './components/learning-resources/LearningResource.vue';
+import StoredResources from './components/learning-resources/StoredResources.vue';
 
 export default {
   components: {
-    LearningResource,
+    StoredResources,
   },
   data() {
     return {
@@ -37,3 +33,19 @@ export default {
   },
 };
 </script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
