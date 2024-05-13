d6aa143575b48d5eaaf8a3c22b45d2dc75c76679
IngDavidTM <david-1011@hotmail.com>
2024-05-13 14:52:18 -0500

    Implement add resources component

---
+++ src/components/learning-resources/TheResources.vue
@@ -0,0 +1,49 @@
+<template>
+  <base-card>
+    <base-button @click="selectTab('stored-resources')">Stored Resources</base-button>
+    <base-button @click="selectTab('add-resource')">Add Resource</base-button>
+  </base-card>
+  <component :is="selectedTab"></component>
+</template>
+
+<script>
+import StoredResources from './StoredResources.vue';
+import AddResource from './AddResource.vue';
+
+
+export default {
+  components: {
+    StoredResources,
+    AddResource
+  },
+  data() {
+    return {
+      selectedTab: 'stored-resources',
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official Vue.js documentation',
+          url: 'https://vuejs.org',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to Google...',
+          url: 'https://google.com',
+        },
+      ],
+    };
+  },
+  provide() {
+    return {
+      resources: this.storedResources,
+    };
+  },
+  methods: {
+    selectTab(tab) {
+      this.selectedTab = tab;
+    }
+  }
+}
+</script>
